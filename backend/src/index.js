f2461e421d55cb1f3197baf9afaa653dacc2057c
uesleirodrigues <ueslei.rodrigues@appsistemas.com.br>
2020-03-31 21:20:36 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const routes  = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET:Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após o "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição utilizado para criar/alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Ueslei Rodrigues'
+    });
+});
+
+app.listen(3333);
